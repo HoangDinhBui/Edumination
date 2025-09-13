5e2044eb69cfc188c70a473a3ae934d599ab8590
HoangBD <buidinhhoang1910@gmail.com>
2025-09-13 19:55:47 +0700

    Feat: Init Frontend

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      // Thêm các alias cần thiết
+    },
+  },
+  define: {
+    global: "window", // Fix for sockjs-client
+  },
+});
