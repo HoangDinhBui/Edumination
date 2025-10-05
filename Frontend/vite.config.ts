0ba51683e1e08f698bd2364f089e5b85d701a521
HoangBD <buidinhhoang1910@gmail.com>
2025-10-05 14:07:16 +0700

    Fix: install tailwindcss version  which compatible with project setup

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -1,16 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      // Thêm các alias cần thiết
-    },
-  },
-  define: {
-    global: "window", // Fix for sockjs-client
-  },
+  plugins: [react()],
 });
